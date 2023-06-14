f26c73ab85eaf8c11c81afe683ef7215f6467f67
Juander28 <85529742+Juander28@users.noreply.github.com>
2023-06-13 21:50:14 -0500

    Update openlane_script.sh

--- openlane_script.sh
+++ openlane_script.sh
@@ -13,8 +13,8 @@ echo
 ORIGIN_LOC=$(pwd)
 sudo apt-get update
 sudo apt-get -y upgrade
-cd
-cd APPS
+mkdir -p work/tools
+cd work/tools
 sudo apt install -y build-essential python3 python3-venv python3-pip
 echo
 echo "=================================="
@@ -52,10 +52,14 @@ echo "=================================="
 echo "----BUILDING OPENLANE----"
 echo "=================================="
 echo
+mkdir openlane_working_dir
+cd openlane_working_dir
 git clone https://github.com/The-OpenROAD-Project/OpenLane.git
 cd OpenLane/
 
 
+cd $ORIGIN_LOC
+chown -R $user_name:$group_name work
 echo
 echo "######CONGRATULATIONS YOU ARE DONE!!########"
 echo
